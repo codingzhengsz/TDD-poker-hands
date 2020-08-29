9a02b3f9dd1802cde9475e4b8e8d86cec1e9e125
Martin <codingzhengsz@163.com>
2020-08-29 15:04:51 +0800

    [PokerHands][Martin & Lester] test: should_return_Tie_when_run_given_pokers_black_3H_3D_5S_9C_KD_white_3S_3C_5H_9D_KS

--- src/test/java/com/example/GameTest.java
+++ src/test/java/com/example/GameTest.java
@@ -272,4 +272,15 @@ public class GameTest {
     // then
     assertEquals("White win. - with Pair", result);
   }
+
+  @Test
+  public void should_return_Tie_when_run_given_pokers_black_3H_3D_5S_9C_KD_white_3S_3C_5H_9D_KS() {
+    // given
+    String black = "3H 3D 5S 9C KD";
+    String white = "3S 3C 5H 9D KS";
+    // when
+    String result = game.run(black, white);
+    // then
+    assertEquals("Tie", result);
+  }
 }
